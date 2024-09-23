8b866b419e8294a02bb29c08b3ba665f0cd690aa
Vipul <vipulwasnik0@gmail.com>
2024-09-23 16:24:41 +0530

    fix margin

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Cart from './components/Cart';
 const App = () => {
   
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 m-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 my-2 mx-4">
       <div className="">
         <AIAssistant />
       </div>
