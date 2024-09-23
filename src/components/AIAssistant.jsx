49821b0efff0dc2afe5f201726387124148f6d51
Vipul <vipulwasnik0@gmail.com>
2024-09-24 01:38:43 +0530

    add logo

--- src/components/AIAssistant.jsx
+++ src/components/AIAssistant.jsx
@@ -18,7 +18,10 @@ const AIAssistant = () => {
 
   return (
     <div className="bg-gray-100 p-4 mb-2 md:m-4 md:py-60 md:min-h-[700px] text-center rounded-lg border-2">
+      <div className='flex justify-center'>
+      <img className="w-8 h-8 rounded-md mx-2" src="https://i.postimg.cc/K8QFJGFw/1705389198122.jpg" alt="logo" /> 
       <h2 className="text-2xl font-bold mb-5">AI Voice Assistant</h2>
+      </div>
       <button
         onClick={handleStart}
         className={`transition-transform duration-150 ease-in-out ${isListening ? 'bg-green-500' : 'bg-blue-500 hover:bg-blue-600'} text-white text-center px-4 py-2 rounded-lg mr-2`}
